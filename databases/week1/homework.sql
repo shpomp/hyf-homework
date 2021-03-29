3ce2d5cff79f4b8e5b1af188656848b77fbbf89b
shpomp <karolina.urnieziute@gmail.com>
2021-03-29 18:38:45 +0200

    DB week1 homework

---
+++ databases/week1/homework.sql
@@ -0,0 +1,91 @@
+1.
+SELECT COUNT(*) as total_rows_in_task
+FROM hyf.task;
+
+-- 35
+
+2.
+SELECT COUNT(*) as total_rows
+FROM hyf.task
+WHERE due_date is null;
+
+-- 8
+
+3.
+SELECT *
+FROM hyf.task
+WHERE status_id = 3;
+
+-- Or
+
+SELECT task.title, status.name
+FROM hyf.task
+    JOIN hyf.status
+    ON task.status_id = status.id
+where status.name = "done";
+
+-- 12 row(s) returned
+
+4.
+SELECT *
+FROM hyf.task
+WHERE status_id != 3;
+
+-- Or
+
+SELECT task.title, status.name
+FROM hyf.task
+    JOIN hyf.status
+    ON task.status_id = status.id
+where status.name != "done";
+
+-- 23 row(s) returned
+
+5.
+SELECT *
+FROM hyf.task
+order by created desc;
+
+-- 2017-10-30 09:47:00 at the top
+
+6.
+SELECT *
+FROM hyf.task
+order by created desc
+limit 1;
+
+7.
+SELECT title
+, due_date
+FROM hyf.task
+WHERE title LIKE "%database%" OR description LIKE "%database%";
+
+-- 5 row(s) returned
+
+8.
+SELECT task.title, status.name
+FROM hyf.task
+    JOIN hyf.status
+    ON task.status_id = status.id;
+
+9.
+SELECT status.name, COUNT(*) as amount
+FROM hyf.task
+    JOIN hyf.status
+    ON task.status_id = status.id
+group by name
+
+-- Done 12
+-- In progress 15
+-- Not started 8
+
+10.
+SELECT s.name
+FROM hyf.status s, hyf.task t
+WHERE s.id = t.status_id
+GROUP BY name
+order by COUNT(s.name) desc
+
+
+
+
---
+++ databases/week1/inClass/inClass.sql
@@ -0,0 +1,146 @@
+SELECT *
+FROM user
+WHERE name like "p_blo%";
+
+SELECT *
+FROM user
+limit
+3, 2
+order by name;
+
+select month(created) as created_on_month, count(id) as no
+from task
+group by created_on_month;
+
+
+
+-- JOINS 
+
+SELECT *
+from task
+    join user on task.user_id = user.id;
+
+SELECT task.id, created, title, user.name
+from task
+    join user on task.user_id = user.id;
+
+
+SELECT task.id, created, title, user.name
+from task
+    left join user on task.user_id = user.id;
+
+SELECT task.id, created, title, user.name, status.name
+from task
+    join user on task.user_id = user.id
+    join status on task.status_id = status.id;
+
+
+SELECT task.id, created, title, user.name
+as user, status.name as status  from task
+ left join user on task.user_id = user.id
+ left join status  on task.status_id = status.id 
+ order by task.id;
+
+SELECT task.id, created, title, user.name
+as user, status.name as status  from task
+  join user on task.user_id = user.id
+  join status  on task.status_id = status.id 
+ order by task.id;
+
+--primary key of the table I want to join in ON 
+
+--charset  utf8mb4  collate 
+
+-- BREAK OUT ROOMS
+
+-- 1. Select the names and phones of all users;
+
+SELECT name, phone
+from user;
+
+-- 2. Select the name of the user with id=10;
+
+SELECT name
+from user
+where user.id = 10;
+
+-- 3. Find how many users exist in the database;
+
+SELECT COUNT(name)
+from user;
+
+-- 4. Select the names of the first 5 users in the database;
+
+SELECT name
+from user
+order by name
+limit 5;
+
+-- 5. Select the names of the last 3 users in the database;
+
+SELECT name
+from user
+order by name
+limit 9, 3;
+
+SELECT name
+from user
+order by id desc
+limit 3;
+
+-- 6. Sum all the ids in the user table;
+
+SELECT sum
+(id)
+from user;
+
+-- 7. Select all users and order them alphabetically by name;
+
+SELECT *
+FROM user
+order by name;
+
+-- 8. Find all tasks that include SQL either on the title or on the description;
+
+SELECT *
+FROM task
+WHERE title LIKE "%SQL%" or description LIKE "%SQL%";
+
+-- 9. Find the title of all tasks that the user Maryrose is responsible for
+
+SELECT task.title
+FROM task JOIN user
+    ON task.user_id = user.id
+WHERE user.name LIKE "Maryrose%";
+
+-- 10. Find how many tasks each user is responsible for;
+
+SELECT count(task.id) as task_count, user.name
+FROM task JOIN user
+    ON task.user_id = user.id
+group by user.name;
+
+-- 11. Find how many tasks with a status=Done each user is responsible for
+
+SELECT count(task.id) as done_tasks, user.name
+FROM task JOIN user
+    ON task.user_id = user.id
+    JOIN status
+    ON task.status_id = status.id
+WHERE status.name = "done"
+group by user.name;
+
+
+-- hw
+
+SELECT status.name, count(task.id) as amount
+FROM hyf.task
+    JOIN hyf.status
+    ON task.status_id = status.id
+group by name
+
+
+
+
+
+
