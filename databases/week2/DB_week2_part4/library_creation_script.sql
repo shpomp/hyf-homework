12f611ba13f53e5979c06b44e45fae2e34b5674d
shpomp <64137994+shpomp@users.noreply.github.com>
2021-04-18 21:49:50 +0200

    Update library_creation_script.sql
    Changed column and foreign key naming logic, corrected statements.

--- databases/week2/DB_week2_part4/library_creation_script.sql
+++ databases/week2/DB_week2_part4/library_creation_script.sql
@@ -1,4 +1,4 @@
-CREATE SCHEMA IF NOT EXISTS `library` DEFAULT CHARACTER SET utf8 ;
+CREATE SCHEMA IF NOT EXISTS `library` DEFAULT CHARACTER SET tf8mb4 ;
 USE `library` ;
 
 -- category`
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS `library`.`category` (
   `description` VARCHAR(45) NOT NULL,
   `shelf_nr` INT NOT NULL,
   PRIMARY KEY (`category_id`))
-ENGINE = InnoDB;
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- book
 CREATE TABLE IF NOT EXISTS `library`.`book` (
@@ -19,13 +19,13 @@ CREATE TABLE IF NOT EXISTS `library`.`book` (
   `publisher` VARCHAR(45) NOT NULL,
   `copies_owned` INT NOT NULL,
   `copies_available` INT NOT NULL,
-  `category_category_id` INT NOT NULL,
+  `category_id` INT NOT NULL,
   PRIMARY KEY (`book_id`),
-  CONSTRAINT `fk_book_category1`
-    FOREIGN KEY (`category_category_id`)
+  CONSTRAINT `fk_book_category`
+    FOREIGN KEY (`category_id`)
     REFERENCES `mydb`.`category` (`category_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
 ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- reader
@@ -49,16 +49,16 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- book_has_author
 CREATE TABLE IF NOT EXISTS `library`.`book_has_author` (
-  `book_book_id` INT NOT NULL,
-  `author_author_id` INT NOT NULL,
-  PRIMARY KEY (`book_book_id`, `author_author_id`),
-  CONSTRAINT `fk_book_has_author_book`
-    FOREIGN KEY (`book_book_id`)
+  `book_id` INT NOT NULL,
+  `author_id` INT NOT NULL,
+  PRIMARY KEY (`book_id`, `author_id`),
+  CONSTRAINT `fk_bookauthor_book`
+    FOREIGN KEY (`book_id`)
     REFERENCES `mydb`.`book` (`book_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_book_has_author_author1`
-    FOREIGN KEY (`author_author_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_bookauthor_author`
+    FOREIGN KEY (`author_id`)
     REFERENCES `mydb`.`author` (`author_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
@@ -67,14 +67,14 @@ ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- book_has_reader`
 CREATE TABLE IF NOT EXISTS `library`.`book_has_reader` (
-  `book_book_id` INT NOT NULL,
-  `reader_reader_id` INT NOT NULL,
-  PRIMARY KEY (`book_book_id`, `reader_reader_id`),
-  CONSTRAINT `fk_book_has_reader_book1` FOREIGN KEY (`book_book_id`)
+  `book_id` INT NOT NULL,
+  `reader_id` INT NOT NULL,
+  PRIMARY KEY (`book_book_id`, `reader_id`),
+  CONSTRAINT `fk_bookreader_book` FOREIGN KEY (`book_id`)
     REFERENCES `mydb`.`book` (`book_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_book_has_reader_reader1` FOREIGN KEY (`reader_reader_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `fk_bookreader_reader` FOREIGN KEY (`reader_id`)
     REFERENCES `mydb`.`reader` (`reader_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
