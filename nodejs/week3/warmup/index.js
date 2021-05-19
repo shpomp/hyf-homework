a3a0387ab294685c975375b5870d2d8b7f11c576
shpomp <64137994+shpomp@users.noreply.github.com>
2021-05-19 14:25:49 +0200

    "/calculator" router

--- nodejs/week3/warmup/index.js
+++ nodejs/week3/warmup/index.js
@@ -1,6 +1,8 @@
 const { response } = require("express");
 const express = require("express");
 const app = express();
+const router = express.Router();
+
 
 app.get("/", (req, res) => res.send("nodejs week3 homework"));
 
@@ -65,5 +67,7 @@ app.get("/divide", (req, res) => {
   console.log("divide");
   res.send(processQuery(divide, req.query));
 });
+
+app.use("/calculator", router);
 
 app.listen(3000, () => console.log(`Calculator:listening on port 3000`));
