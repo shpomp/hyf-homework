c39e944d4c4fca61df0e2b0f02cefc5074e4cd86
shpomp <64137994+shpomp@users.noreply.github.com>
2021-06-06 22:21:55 +0200

    Delete react/week3/prep-app directory

--- react/week3/prep-app/src/ChildrenExample.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState, useCallback } from "react";
-
-const ChildrenExample = (props) => {
-  const { children } = props;
-
-  return <div></div>;
-};
-
-export default ChildrenExample;
